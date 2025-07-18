afea7cbafbe65800c49aae03fc0b7d26f0c46e89
vas0x59 <vasily.yuryev.a1@gmail.com>
2025-07-18 20:20:42 +0300

    Revert "Add memory statistics on standard_viewer (#226)"
    This reverts commit 1591f046e7b63362cb4cde0b63481fa4371b8576.

--- src/glim/mapping/global_mapping.cpp
+++ src/glim/mapping/global_mapping.cpp
@@ -362,7 +362,7 @@ void GlobalMapping::optimize() {
     return;
   }
 
-  logger->debug("|new_factors|={} |new_values|={}", new_factors->size(), new_values->size());
+  logger->info("|new_factors|={} |new_values|={}", new_factors->size(), new_values->size());
 
   Callbacks::on_smoother_update(*isam2, *new_factors, *new_values);
   auto result = update_isam2(*new_factors, *new_values);
